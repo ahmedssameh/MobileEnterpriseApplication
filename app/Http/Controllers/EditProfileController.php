986f14c842dd485c7e5b2b8d2ea76026b26faa9c
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-11 01:33:28 +0300

    change

--- app/Http/Controllers/EditProfileController.php
+++ app/Http/Controllers/EditProfileController.php
@@ -14,11 +14,9 @@ use App\Models\User;
 class EditProfileController extends Controller
 {
 
-    public function update(Request $request)
-    {
+    public function update(Request $request){
         $user = auth()->user();
 
-        // Validate the request data
         $validatedData = $request->validate([
             'name' => 'required',
             'contact_person_name' => 'required',
@@ -27,19 +25,14 @@ class EditProfileController extends Controller
             'company_size' => 'required',
         ]);
 
-        // Update the user's information
-        $user->name = $validatedData['name'];
-        $user->contact_person_name = $validatedData['contact_person_name'];
-        $user->contact_person_phone_number = $validatedData['contact_person_phone_number'];
-        $user->company_address = $validatedData['company_address'];
-        $user->company_size = $validatedData['company_size'];
-        // ...
 
-        // Save the updated user record
-        $user->save();
+        $user->update($validatedData);
 
-        // Return a response indicating success
-        return response()->json(['message' => 'User information updated successfully']);
+        return response()->json(['message'=> 'User is updated',
+            'user'=> $user
+        ],
+            201
+        );
     }
 
 }
