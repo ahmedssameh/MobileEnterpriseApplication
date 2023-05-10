13b9e248bae92858875de407ce889107673938f1
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-11 01:20:50 +0300

    change

--- app/Http/Controllers/EditProfileController.php
+++ app/Http/Controllers/EditProfileController.php
@@ -8,34 +8,38 @@ use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Contracts\Providers\Auth;
 use App\Models\User;
 
+
+
+
 class EditProfileController extends Controller
 {
 
-    public function update(Request $request){
-
-        $validatedData = Validator::make($request->all(),[
-            'name'=>'required',
-            'contact_person_name'=>'required',
-            'contact_person_phone_number'=> 'required',
-            'company_address'=>'required',
-            'company_size'=>'required',
-        ]);
-
-
-
-        if($validatedData->fails()){
-            $errorString = implode("\n", $validatedData->errors()->all());
-            return response()->json(['details'=>$errorString],400)->header('Content-Type', 'application/json');
-        }
-
+    public function update(Request $request)
+    {
         $user = auth()->user();
 
-        $user->update(array_merge($validatedData->validated()));
+        // Validate the request data
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'contact_person_name' => 'required',
+            'contact_person_phone_number' => 'required',
+            'company_address' => 'required',
+            'company_size' => 'required',
+        ]);
 
-        return response()->json(['message'=> 'User is updated',
-            'user'=> $user
-        ],
-            201
-        );
+        // Update the user's information
+        $user->name = $validatedData['name'];
+        $user->contact_person_name = $validatedData['contact_person_name'];
+        $user->contact_person_phone_number = $validatedData['contact_person_phone_number'];
+        $user->company_address = $validatedData['company_address'];
+        $user->company_size = $validatedData['company_size'];
+        // ...
+
+        // Save the updated user record
+        $user->save();
+
+        // Return a response indicating success
+        return response()->json(['message' => 'User information updated successfully']);
     }
+
 }
