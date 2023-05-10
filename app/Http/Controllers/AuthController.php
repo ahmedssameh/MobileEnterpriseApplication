6e70b5c7160cb106879cf6061869c943dc2e6029
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-10 21:15:27 +0300

    change

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
 
 
         if($validator->fails()){
-           return response()->json($validator->errors()->toJson(),400);
+           return response()->json($validator->errors()->toJson(),400)->header('Content-Type', 'application/json');
         }
 
         $user = User::create(array_merge($validator->validated(),
