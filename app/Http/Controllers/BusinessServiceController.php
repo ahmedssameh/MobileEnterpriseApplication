987a69268f6940c619c3dc21b90687385d6305cc
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-16 02:27:09 +0300

    change

--- app/Http/Controllers/BusinessServiceController.php
+++ app/Http/Controllers/BusinessServiceController.php
@@ -96,7 +96,7 @@ class BusinessServiceController extends Controller
         if ($businessService) {
 
             return response()->json(['message' => 'The company profile of this service',
-                'Company Profile' =>$user
+                'Company Profile' =>$businessService
             ],
                 201
             );
