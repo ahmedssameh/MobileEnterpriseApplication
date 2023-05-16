312c11ad33c83dd4c223a4f43649ef89cf11ee71
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-16 04:02:00 +0300

    change

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
+
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
