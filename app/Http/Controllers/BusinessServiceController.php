4d3dea2425d29ad83f8af35078e047a5cb327fe8
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-16 04:53:37 +0300

    change

--- app/Http/Controllers/BusinessServiceController.php
+++ app/Http/Controllers/BusinessServiceController.php
@@ -121,7 +121,7 @@ class BusinessServiceController extends Controller
             return response()->json(['details' => $errorString], 400)->header('Content-Type', 'application/json');
         }
 
-        $user = User::where('id',$validator->validated()['id'])->first();
+        $user = User::find($validator->validated()['id']);
         //$user=$businessService->user_id;
         if ($user) {
 
--- database/migrations/2023_05_15_215955_fav_service.php
+++ database/migrations/2023_05_15_215955_fav_service.php
@@ -15,9 +15,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('service_id');
             $table->timestamps();
-
             $table->primary(['user_id', 'service_id']);
-
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('service_id')->references('id')->on('business_service');
         });
