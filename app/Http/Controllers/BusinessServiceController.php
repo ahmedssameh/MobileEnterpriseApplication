14b7c2a42b0ca5e84d0ba59add58b98e0b69325e
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2023-05-19 21:11:03 +0300

    change

--- app/Http/Controllers/BusinessServiceController.php
+++ app/Http/Controllers/BusinessServiceController.php
@@ -115,7 +115,7 @@ class BusinessServiceController extends Controller
                     $userName = $user->name;
                     $userPhoto = $user->photo;
 
-                    $myservice= business_service::find($service->id);
+                    $myservice= business_service::find($service->servic_id);
 
                     $data[] = [
                         'service_id' => $myservice->id,
